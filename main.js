e624be5fcddd65d17d778e490752b2a90dc457c2
hieesu-dev <hieulethanh.dev@gmail.com>
2022-12-09 02:18:27 +0700

    update site's info

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import blog, { ga, redirects } from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
-  title: "Hieesu",
+  title: "Hieesu's Blog",
+  // description: "The blog description.",
   author: "Hieesu",
   avatar: "https://avatars.githubusercontent.com/u/68918968?v=4",
-  avatarClass: "full",
+  avatarClass: "rounded-full",
   links: [
     { title: "Email", url: "mailto:hieesu.me@gmail.com" },
     { title: "GitHub", url: "https://github.com/hieesu-dev" },
