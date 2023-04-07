d9491ff61f6bfb2e278251fb0d2516d514536d7f
hieesu-dev <hieulethanh.dev@gmail.com>
2023-04-08 03:22:11 +0700

    update site

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 import blog, { ga, redirects } from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
-  title: "Hieesu's Blog",
-  // description: "The blog description.",
+  title: "Hieu Le",
+  description: "Software Engineer.",
   author: "Hieesu",
   avatar: "https://avatars.githubusercontent.com/u/68918968?v=4",
   avatarClass: "rounded-full",
   links: [
     { title: "Email", url: "mailto:hieesu.me@gmail.com" },
-    { title: "GitHub", url: "https://github.com/hieesu-dev" },
+    { title: "GitHub", url: "https://github.com/hieesume" },
+    { title: "CV", url: "https://docs.google.com/document/d/1c9LXz4Guwi-LC-kzsB-5CtWiOax-FddIkF4V3agSa_w/edit?usp=sharing" },
   ],
   background: "#fff",
   middlewares: [
